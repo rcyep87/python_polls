a139b8a4ad024995e7b70c28fb2259294b85f9cc
Ryan Yep <ryan.yep@gmail.com>
2016-01-22 13:16:40 -0600

    Created Question and Choice models successfully

--- polls/settings.py
+++ polls/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pollss.apps.PollssConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
